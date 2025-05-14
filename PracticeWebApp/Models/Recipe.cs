1f134526b8cc84e5c4c89b46ca54eb1cff572977
jmm10 <jmm10@JOHNLT>
2025-05-14 12:38:48 -0600

    Add project files.

---
+++ PracticeWebApp/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿namespace PracticeWebApp.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string ingredients{ get; set; }
+
+        public string cookingInstructions { get; set; }
+
+        public string dishName { get; set; }
+
+        public Recipe()
+        {
+                
+        }
+    }
+}
